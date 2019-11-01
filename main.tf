454dd5eb651e1d0e170837dc57bc16f20c3cf437
etwillbefine <tim.gatzemeier@gmx.de>
2019-11-01 17:29:40 +0100

    correct reference to resource with count set

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "tls_private_key" "private_key" {
 
 resource "acme_registration" "reg" {
   count            = var.enabled ? 1 : 0
-  account_key_pem = join("", tls_private_key.private_key.private_key_pem)
+  account_key_pem = join("", tls_private_key.private_key.*.private_key_pem)
   email_address   = var.certificate_email
 }
 
