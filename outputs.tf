db61ab7c9efda3eded36d926de5fea86baea6aea
etwillbefine <tim.gatzemeier@gmx.de>
2019-11-03 14:50:04 +0100

    adjust output names, add ca_key output

--- outputs.tf
+++ outputs.tf
@@ -12,8 +12,13 @@ output "certificate_body" {
   value = join("", acme_certificate.certificate.*.certificate_pem)
 }
 
-output "certificate_chain" {
+output "certificate_ca_cert" {
   value = join("", acme_certificate.certificate.*.issuer_pem)
+}
+
+output "certificate_ca_key" {
+  value     = local.account_key_pem
+  sensitive = true
 }
 
 output "certificate_arn" {
